f64374ee7507ca7880d13c9818d9d1762f576ac4
Mike Kasprzak <mike@distraction.engineer>
2025-06-25 13:21:46 -0400

    Os is best optimization choice for size

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 emcc hello.cpp -o hello/hello.html
-emcc -O2 hello.cpp -o hello/hello-o2.html
-gzip -fk hello/hello-o2.wasm
+emcc -Os hello.cpp -o hello/hello-Os.html
+gzip -fk hello/hello-Os.wasm
 
 emcc cube.cpp -o cube/cube.html
-emcc -O2 cube.cpp -o cube/cube-o2.html
-gzip -fk cube/cube-o2.wasm
+emcc -Os cube.cpp -o cube/cube-Os.html
+gzip -fk cube/cube-Os.wasm
 
 # Add file to virtual filesystem
 emcc file.cpp -o file/file.html --preload-file file/file.txt
-emcc -O2 file.cpp -o file/file-o2.html --preload-file file/file.txt
-gzip -fk file/file-o2.wasm
+emcc -Os file.cpp -o file/file-Os.html --preload-file file/file.txt
+gzip -fk file/file-Os.wasm
 # Embed file in the wasm, adding it to the virtual filesystem
 emcc file.cpp -o file/embed.html --embed-file file/file.txt
-emcc -O2 file.cpp -o file/embed-o2.html --embed-file file/file.txt
-gzip -fk file/embed-o2.wasm
+emcc -Os file.cpp -o file/embed-Os.html --embed-file file/file.txt
+gzip -fk file/embed-Os.wasm
